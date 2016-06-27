42132068c1b68d7c261e5a27905683935b24654b
soychileno <fentiaro@hotmail.com>
2016-06-26 23:41:47 -0300

    sd
    asd

--- application/controllers/landing_page.php
+++ application/controllers/landing_page.php
@@ -23,4 +23,9 @@ class landing_page extends CI_Controller{
             $this->load->view('login');
         }
     }
+    public function login_post(){
+        if(session_start())
+        $this->load->view('productos/login_post');
+    }
+    
 }
